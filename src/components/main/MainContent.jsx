a920f4bda4543622e2105e959677f2909f3af0ff
asell09 <miniinasel@gmail.com>
2024-09-03 13:23:38 +0600

    first commit

--- src/components/main/MainContent.jsx
+++ src/components/main/MainContent.jsx
@@ -13,19 +13,21 @@ function MainComponent () {
       </div>
       <div className="right-section">
         <div className="checkbox-item">
-          <input type="checkbox"/>
-          <label>Lorem ipsum dolor sit amet consectetur.<br /> Sed lacus condimentum est lorem id.</label>
+          <input type="checkbox"  />
+          <label>Lorem ipsum dolor sit amet consectetur. <br /> Sed lacus  condimentum est lorem id.</label>
         </div>
         <div className="checkbox-item">
           <input type="checkbox"/>
-          <label>Lorem ipsum dolor sit amet consectetur.<br /> Sed lacus condimentum est lorem id.</label>
+          <label>Lorem ipsum dolor sit amet consectetur. <br /> Sed lacus condimentum est lorem id.</label>
         </div>
         <div className="checkbox-item">
-          <input type="checkbox"/>
-          <label>Lorem ipsum dolor sit amet consectetur.<br /> Sed lacus condimentum est lorem id.</label>
+          <input type="checkbox" className='chek-in'/>
+          <label>Lorem ipsum dolor sit amet consectetur. <br /> Sed lacus condimentum est lorem id.</label>
         </div>
       </div>
     </div>
+
+
     <div className="asi-conteiner">
       <div className="image-container">
         <img src={img23} alt="Delivery" />
@@ -33,7 +35,7 @@ function MainComponent () {
       <div className="text-container">
         <h2>Lorem ipsum dolor sit amet consectetur.</h2>
         <p>
-        Lorem ipsum dolor sit amet consectetur.<br /> Platea suscipit suscipit tristique enim id. At molestie aliquet fermentum erat euismod sit ut viverra.
+        Lorem ipsum dolor sit amet consectetur.<br /> Platea suscipit suscipit tristique enim id. At molestie aliquet fermentum erat euismod sit ut viverra. <br />
          Sit sed blandit ullamcorper donec quam sed. .
         </p>
         <button className="learn-more">Learn more</button>
