7e6a55bcfb05d553920468f1ee83ed001c66372d
asell09 <miniinasel@gmail.com>
2024-09-16 12:40:14 +0600

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,20 +4,10 @@ import img2 from './../imgs/rahmat1.png'
 
 function Header() {
 
-  // const nav = document.querySelector('#nav')
-  // const navBtn = document.querySelector('#nav-btn')
-  // const navBtnImg = document.querySelector('#nav-btn-img')
-
-  //         navBtn.onClick = () => {
-  //           if (nav.classList.toggle('open')) {
-  //             navBtnImg.src = './img/icons/nav-close.svg';
-  //           } else {
-  //             navBtnImg.src = './img/icons/nav-open.svg';
-  //           }
-  //         }
 
   return (
     <>
+    
       <div className='header-row'>
         <nav id='nav' className="nav open">
           <div className="logo">
